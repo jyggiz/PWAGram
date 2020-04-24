c8ab0596caaf1e391f124d440ae391f9c573604c
Тимур Жигмытов <timur.zhigmytov@innoforce.kz>
2020-04-24 21:06:34 +0600

    Add example of fetching data

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -16,4 +16,16 @@ window.addEventListener('beforeinstallprompt', function(event) {
   event.preventDefault();
   deferredPrompt = event;
   return false;
-})
+});
+
+fetch("https://httpbin.org/ip")
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
