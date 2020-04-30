4c9acf650c325d0be557884e7b89ba7ad6261178
Timur Zhigmytov <zhygmytov@gmail.com>
2020-04-30 22:34:31 +0600

    Add functional for fetching posts in offline mode from indexedDB

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -112,21 +112,12 @@ fetch(url)
     updateUI(dataArray);
   });
 
-if ('caches' in window) {
-  caches.match(url)
-    .then(function (response) {
-      if (response) {
-        return response.json();
-      }
-    })
+if ('indexedDB' in window) {
+  readAllData('posts')
     .then(function(data) {
-      console.log("From Cache", data);
       if (!networkDataReceived) {
-        var dataArray = [];
-        for(var key in data) {
-          dataArray.push(data[key]);
-        }
-        updateUI(dataArray);
+        console.log('From indexedDB', data);
+        updateUI(data);
       }
     })
 }
