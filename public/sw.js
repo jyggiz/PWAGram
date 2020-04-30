6791d847667f3f5d98e652b342eea1c3bbbb8056
Timur Zhigmytov <zhygmytov@gmail.com>
2020-04-30 22:20:06 +0600

    Add indexedDB to service Worker and save posts request in indexedDB

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,6 @@
-var CACHE_STATIC_NAME = 'static-v15';
+importScripts('/src/js/idb.js');
+
+var CACHE_STATIC_NAME = 'static-v16';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
   '/',
@@ -6,6 +8,7 @@ var STATIC_FILES = [
   '/offline.html',
   '/src/js/app.js',
   '/src/js/feed.js',
+  '/src/js/idb.js',
   '/src/js/promise.js',
   '/src/js/fetch.js',
   '/src/js/material.min.js',
@@ -16,6 +19,12 @@ var STATIC_FILES = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
   ];
+
+var dbPromise = idb.open('posts-store', 1, function (db) {
+  if (!db.objectStoreNames.contains('posts')) {
+    db.createObjectStore('posts', { keyPath: 'id' });
+  }
+});
 
 /*function trimCache(cacheName, maxItems) {
   caches.open(cacheName)
@@ -100,14 +109,22 @@ self.addEventListener('fetch', function(event) {
 
   if (event.request.url.indexOf(url) > -1) {
     event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME).
-        then(function(cache) {
-          return fetch(event.request)
-            .then(function(res) {
-              //trimCache(CACHE_DYNAMIC_NAME, 3);
-              cache.put(event.request.url, res.clone());
-              return res;
+      fetch(event.request)
+        .then(function(res) {
+          var clonedRes = res.clone();
+          clonedRes.json()
+            .then(function(data) {
+              for(var key in data) {
+                dbPromise
+                  .then(function(db) {
+                    var tx = db.transaction('posts', 'readwrite');
+                    var store = tx.objectStore('posts');
+                    store.put(data[key]);
+                    return tx.complete;
+                  })
+              }
             })
+          return res;
         })
     );
   } else if (isInArray(url, STATIC_FILES)) {
