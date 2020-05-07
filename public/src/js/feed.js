94e203f22d1db93e3d526685c91fdb429cd2f3b5
Timur Zhigmytov <zhygmytov@gmail.com>
2020-05-07 21:56:24 +0600

    Add functional for tacking picture in create post page

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -40,6 +40,17 @@ function initializeMedia() {
     })
 }
 
+captureButton.addEventListener('click', function(event) {
+  canvas.style.display = "block";
+  videoPlayer.style.display = "none";
+  captureButton.style.display = "none";
+  var context = canvas.getContext('2d');
+  context.drawImage(videoPlayer, 0, 0, canvas.width, videoPlayer.videoHeight / (videoPlayer.videoWidth / canvas.width));
+  videoPlayer.srcObject.getVideoTracks().forEach(function(track) {
+    track.stop();
+  });
+});
+
 function openCreatePostModal() {
     createPostArea.style.transform = "translateY(0)";
     initializeMedia();
