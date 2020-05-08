79533a6202a8ecfb3f59ac2f7bf3881cb91bd6e8
Timur Zhigmytov <zhygmytov@gmail.com>
2020-05-08 13:29:07 +0600

    Add fallback for video camera feature

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -53,6 +53,10 @@ captureButton.addEventListener('click', function(event) {
   picture = dataURItoBlob(canvas.toDataURL());
 });
 
+imagePicker.addEventListener('change', function(event) {
+  picture = event.target.files[0];
+})
+
 function openCreatePostModal() {
     createPostArea.style.transform = "translateY(0)";
     initializeMedia();
